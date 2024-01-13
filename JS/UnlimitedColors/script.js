fbec08786461a8afe5229da6a949886b040eef9e
Navya Bhandari <navyabhandari0@gmail.com>
2024-01-14 02:39:14 +0530

    .

---
+++ JS/UnlimitedColors/script.js
@@ -0,0 +1,23 @@
+// generate a random number
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#';
+    for(let i=0; i<6; i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+let intervalId;
+const startChange = function(){
+    intervalId = setInterval(changeBg,1000)
+    function changeBg(){
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+const stopChange = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+document.querySelector('#start').addEventListener('click',startChange)
+document.querySelector('#stop').addEventListener('click',stopChange)
